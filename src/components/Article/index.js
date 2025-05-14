f4170effb53907a6868b92d015b1c0664984eada
contato <contato@prosoccerapp.com.br>
2025-05-14 10:33:16 -0300

    layout blog

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -2,11 +2,39 @@
 import Post from '../Post';
 
 const Article = () => {
+    //fazer uma requisicao para uma API
+
     return (
-        <>
-        <h1>Article.js</h1>
-        <Post/>
-        </>
+    <section className='container'>
+        <h1>Articles</h1>
+        <div className='mt-5 container-posts'>
+        <Post subtitle = "tecnologia" title = "O guia definitivo do blog">
+            1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna 
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Duis aute irure dolor in reprehenderit in voluptate velit esse 
+          cillum dolore eu fugiat nulla pariatur.
+        </Post>
+        <Post subtitle = "fotografia" title = "Quais as melhores cameras em 2021?">
+            2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna 
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Duis aute irure dolor in reprehenderit in voluptate velit esse 
+          cillum dolore eu fugiat nulla pariatur.
+        </Post>
+        <Post subtitle = "cinema" title = "Os 10 filmes com maior bilheteria">
+            3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna 
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Duis aute irure dolor in reprehenderit in voluptate velit esse 
+          cillum dolore eu fugiat nulla pariatur.
+        </Post>
+        </div>
+        
+    </section>
     )
 }
 
